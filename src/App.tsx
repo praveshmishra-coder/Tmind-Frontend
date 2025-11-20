b780c599ed399c078290aba6ce6a42f3d858c100
Varad Dhumale <Varad Dhumale@DESKTOP-QDF6RTS>
2025-11-20 16:28:34 +0530

    port form added

--- src/App.tsx
+++ src/App.tsx
@@ -15,33 +15,38 @@ import Reports from "./pages/Reports";
 import Settings from "./pages/Settings";
 import DeletedDevices from "./devices/DeletedDevices";
 import Profile from "./pages/Profile";
+import AddPortForm from "./pages/AddPortsForm";
+import { TooltipProvider } from "@/components/ui/tooltip"; // from your shadcn tooltip file
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        
-        <Route path="/" element={<Login />} />
+    <TooltipProvider>
+      <Router>
+        <Routes>
 
-        <Route element={<DashboardLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/assets" element={<Assets />} />
-          <Route path="/devices" element={<Devices />} />
-          <Route path="/devices/add" element={<AddDeviceForm />} />
-          <Route path="/devices/edit/:deviceId" element={<EditDeviceForm />} />
-          <Route path="/devices/config/:deviceId" element={<ConfigureDeviceForm/>} />
-          <Route path="/devices/ports" element={<PortSettings/>} />
-          <Route path="/devices/upload" element={<UploadCsv/>} />
-          <Route path="/signals" element={<Signals />} />
-          <Route path="/reports" element={<Reports />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/deleted-devices" element={<DeletedDevices />} />
-          <Route path="/profile" element={<Profile />} />
-        </Route>
+          <Route path="/" element={<Login />} />
 
-        {/* Redirect unknown routes to login */}
-        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
-      </Routes>
-    </Router>
+          <Route element={<DashboardLayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/assets" element={<Assets />} />
+            <Route path="/devices" element={<Devices />} />
+            <Route path="/devices/add" element={<AddDeviceForm />} />
+            <Route path="/devices/edit/:deviceId" element={<EditDeviceForm />} />
+            <Route path="/devices/config/:deviceId" element={<ConfigureDeviceForm />} />
+            <Route path="/devices/ports" element={<PortSettings />} />
+            <Route path="/devices/ports/:id" element={<AddPortForm />} />
+            <Route path="/devices/upload" element={<UploadCsv />} />
+            <Route path="/signals" element={<Signals />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/deleted-devices" element={<DeletedDevices />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+
+          {/* Redirect unknown routes to login */}
+          {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+        </Routes>
+      </Router>
+    </TooltipProvider>
   );
 }
